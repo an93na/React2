9de2fcecd5273c0f25146c8a071a236da51cfdf3
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-03 15:43:58 +0200

    changes

--- homework1/src/componentsPrzelicz/AppTitle.js
+++ homework1/src/componentsPrzelicz/AppTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 import { WeightFrom } from "./WeightFrom";
 
 export const AppTitle = () => {
-    return <article style={{backgroundColor: '#8D8D8D', width: 500, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '0 auto'}}>
+    return <article style={{backgroundColor: '#8D8D8D', width: 500, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '0 auto', marginTop: 10}}>
       <div>
           <h3>Weight Converter</h3>
       </div>
--- homework1/src/componentsPrzeliczOdl/AppTitle.js
+++ homework1/src/componentsPrzeliczOdl/AppTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const AppTitleDist = () => {
   return (
-    <article>
+    <article style={{backgroundColor: '#8D8D8D', width: 500, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '0 auto', marginTop: 10}}>
       <div>
         <h3>Distance Converter</h3>
       </div>
