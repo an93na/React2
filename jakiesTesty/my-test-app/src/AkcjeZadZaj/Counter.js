760b5dd6b859bd8c4a09f13ee7baa3f85527884e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-06 12:39:52 +0200

    changes

--- jakiesTesty/my-test-app/src/AkcjeZadZaj/Counter.js
+++ jakiesTesty/my-test-app/src/AkcjeZadZaj/Counter.js
@@ -3,8 +3,9 @@ import React, { useState } from "react";
 
 
 export const Counter = () => {
-    const [number, setNumber] = useState('')
+    const [number, setNumber] = useState(0)
     return <article>
+        <h4>Counter czyli element dodający/odejmujacy 1 od wpisanej liczby</h4>
         <button onClick={()=> setNumber(number+1)}>+1</button>
         <input type="text" name="" id="" value={number} onChange={(event) => setNumber(Number(event.target.value))}/>
         <button onClick={() => setNumber(number-1)}>-1</button>
