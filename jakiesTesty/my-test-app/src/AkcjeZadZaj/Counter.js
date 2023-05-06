3256f1b7df6622e1e86da20cab3cddd5e91b9917
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-06 12:37:16 +0200

    changes

--- jakiesTesty/my-test-app/src/AkcjeZadZaj/Counter.js
+++ jakiesTesty/my-test-app/src/AkcjeZadZaj/Counter.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 export const Counter = () => {
+    const [number, setNumber] = useState('')
     return <article>
-        
+        <button onClick={()=> setNumber(number+1)}>+1</button>
+        <input type="text" name="" id="" value={number} onChange={(event) => setNumber(Number(event.target.value))}/>
+        <button onClick={() => setNumber(number-1)}>-1</button>
     </article>
 }
--- jakiesTesty/my-test-app/src/App.js
+++ jakiesTesty/my-test-app/src/App.js
@@ -1,10 +1,11 @@
+import { Counter } from './AkcjeZadZaj/Counter';
 import './App.css';
 import '@picocss/pico'
 
 function App() {
   return (
     <div className="App">
-    
+    <Counter />
     </div>
   );
 }
