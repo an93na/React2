056e08a2a30296ea8a0f497f4c4c3912290c2019
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-06 12:43:02 +0200

    changes

---
+++ jakiesTesty/my-test-app/src/AkcjeZadZaj/KalkulatorZFormularzem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+export const KalkulatorZFormularzem = () => {
+    return <article>
+        <h4>Kalkulator z formularza</h4>
+    </article>
+}
--- jakiesTesty/my-test-app/src/App.js
+++ jakiesTesty/my-test-app/src/App.js
@@ -1,4 +1,5 @@
 import { Counter } from './AkcjeZadZaj/Counter';
+import { KalkulatorZFormularzem } from './AkcjeZadZaj/KalkulatorZFormularzem';
 import './App.css';
 import '@picocss/pico'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
     <Counter />
+    <KalkulatorZFormularzem />
     </div>
   );
 }
