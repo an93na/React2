262b22570ed2d413ca826b267c3f3b7e1791947f
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-06 14:10:47 +0200

    changes

--- jakiesTesty/my-test-app/src/AkcjeZadZaj/Formularz.js
+++ jakiesTesty/my-test-app/src/AkcjeZadZaj/Formularz.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 export const Formularz = () => {
+    const [title, setTitle] = useState('');
+    const [priority, setPriority] = useState('');
+    const [task, setTask] = useState([])
     return <article>
-
+        <h4>Formularz</h4>
+        <form action="" onSubmit={(e) => {e.preventDefault()
+        const newTask = {
+            title,
+            priority,
+        }
+        setTask((prevTask)=> [...prevTask, newTask]);
+        setTitle('');
+        setPriority('');
+        }}>
+            <input type="text" name="" id="" placeholder="Task title..." value={title} onChange={(event) => setTitle(event.target.value)}/>
+            <select name="" id="" value={priority} onChange={(event) => setPriority(event.target.value)}>
+                <option value="" hidden>Select priority</option>
+                <option value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+            </select>
+            <button type="submit">Submit</button>
+        </form>
+        <pre style={{textAlign:'left'}}>{JSON.stringify(task, null, 2)}</pre>
     </article>
 }
