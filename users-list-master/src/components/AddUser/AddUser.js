e77d98c633cc481dd2fcb315e350ee355f5962f5
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-07 19:59:58 +0200

    changes

--- users-list-master/src/components/AddUser/AddUser.js
+++ users-list-master/src/components/AddUser/AddUser.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
 export const AddUser = () => {
-    const [plec, setPlec] = useState('');
-    const [name, setName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [city, setCity] = useState('');
-    const [country, setCountry] = useState('');
-
+  const [plec, setPlec] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+    console.log(plec);
   return (
-    <div style={{backgroundColor: '#276ace', color:'white'}}>
+    <div style={{ backgroundColor: "#276ace", color: "white" }}>
       <h4 style={{ padding: 10 }}>Dodaj nowego użytkownika</h4>
-      <form action="" style={{ padding: 10 }} onSubmit={(e)=> {e.preventDefault()}}>
-        <select name="" id="">
+      <form
+        action=""
+        style={{ padding: 10 }}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <select name="" id="" value={plec} onChange={(event) => setPlec(event.target.value)}>
           <option value="" hidden>
             Wybierz płeć
           </option>
@@ -19,10 +25,10 @@ export const AddUser = () => {
           <option value="M">mężczyzna</option>
         </select>
         <div>
-          <input type="text" name="" id="" placeholder="Wpisz imię" />
-          <input type="text" name="" id="" placeholder="Wpisz nazwisko" />
-          <input type="text" name="" id="" placeholder="Wpisz miasto" />
-          <input type="text" name="" id="" placeholder="Wpisz kraj" />
+          <input type="text" name="" id="" placeholder="Wpisz imię" value={name} onChange={(event)=> setName(event.target.value)}/>
+          <input type="text" name="" id="" placeholder="Wpisz nazwisko" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+          <input type="text" name="" id="" placeholder="Wpisz miasto" value={city} onChange={(event) => setCity(event.target.value)}/>
+          <input type="text" name="" id="" placeholder="Wpisz kraj" value={country} onChange={(event) => setCountry(event.target.value)}/>
         </div>
         <button type="submit">Dodaj</button>
       </form>
