bdd81b70bc3c412b62a01f098ef83e86af328cfa
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-07 14:15:06 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/FilterProducts/SearchBar.js
+++ my-test-app/src/AkcjeZadZaj/FilterProducts/SearchBar.js
@@ -4,6 +4,12 @@ import React from "react";
 
 export const SearchBar = () => {
     return <div>
+        <form>
+            <input type="text" name="" id="" placeholder="Search ..."/>
+            <label>
+                <input type="checkbox" name="" id="" /> Only show products in stock
+            </label>
+        </form> 
         
     </div>
 }
