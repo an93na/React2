2d8458f11c557635106d7422ded389eab73aedb8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-07 14:10:32 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/FilterProducts/FilterableProductTable.js
+++ my-test-app/src/AkcjeZadZaj/FilterProducts/FilterableProductTable.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { SearchBar } from './SearchBar'
+import { ProductTable } from './ProductTable'
 
 
 
 export const FilterableProductTable = () => {
     return <article style={{marginLeft: 30, marginRight: 30}}>
         <h4>FilterableProductTable</h4>
+        <SearchBar />
+        <ProductTable />
     </article>
 }
--- my-test-app/src/AkcjeZadZaj/FilterProducts/ProductTable.js
+++ my-test-app/src/AkcjeZadZaj/FilterProducts/ProductTable.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { ProductCategoryRow } from "./ProductCategoryRow";
+import { ProductRow } from "./ProductRow";
 
 
 
 export const ProductTable = () => {
     return <div>
-        
+        <ProductCategoryRow />
+        <ProductRow />
     </div>
 }
