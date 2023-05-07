936327c3019b347749581de04af7365c4d3a2d87
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-07 13:20:07 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/Calculator2.js
+++ my-test-app/src/AkcjeZadZaj/Calculator2.js
@@ -1,39 +1,55 @@
 import React, { useState } from "react";
 
 export const Calculator2 = () => {
-    const [number, setNumber] = useState(0)
-    const [operation, setOpration] = useState('')
+    const [expression, setExpression] = useState('')
+    const [result, setResult] = useState(0)
+
+    const handleButtonClick = (buttonValue) => {
+        if (buttonValue === 'AC') {
+            setExpression('');
+            setResult(0)
+        }
+        else if (buttonValue === '=') {
+            setResult(eval(expression));
+            setExpression('')
+        }
+        else {
+            setExpression((prevExpression) => prevExpression + buttonValue)
+        }
+    }
+
   return (
     <article style={{ marginLeft: 30, marginRight: 30 }}>
       <h4>Kalkulator 2</h4>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
       <div style={{width: 400, backgroundColor: "gray", padding: 20, margin: 20, borderRadius: '10px'}}>
-        <input type="text" name="" id="" readOnly value={`${number}${operation}${number}`} />
+      <input type="text" name="" id="" readOnly value={result || expression} />
         <div style={{ display: "flex", gap: 20 }}>
-          <button onClick={()=> setNumber(1)}>1</button>
-          <button onClick={() => setNumber(2)}>2</button>
-          <button onClick={() => setNumber(3)}>3</button>
+          <button onClick={()=> handleButtonClick(1)}>1</button>
+          <button onClick={() => handleButtonClick(2)}>2</button>
+          <button onClick={() => handleButtonClick(3)}>3</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button onClick={()=> setNumber(4)}>4</button>
-          <button onClick={() => setNumber(5)}>5</button>
-          <button onClick={() => setNumber(6)}>6</button>
+          <button onClick={()=> handleButtonClick(4)}>4</button>
+          <button onClick={() => handleButtonClick(5)}>5</button>
+          <button onClick={() => handleButtonClick(6)}>6</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button onClick={()=> setNumber(7)}>7</button>
-          <button onClick={() => setNumber(8)}>8</button>
-          <button onClick={() => setNumber(9)}>9</button>
+          <button onClick={()=> handleButtonClick(7)}>7</button>
+          <button onClick={() => handleButtonClick(8)}>8</button>
+          <button onClick={() => handleButtonClick(9)}>9</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button onClick={()=> setOpration('+')}>+</button>
-          <button onClick={()=> setOpration('-')}>-</button>
-          <button onClick={()=> setOpration('*')}>*</button>
+          <button onClick={()=> handleButtonClick('+')}>+</button>
+          <button onClick={()=> handleButtonClick('-')}>-</button>
+          <button onClick={()=> handleButtonClick('*')}>*</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button onClick={()=> setOpration('/')}>/</button>
-          <button>=</button>
-          <button onClick={()=> {setOpration('')
-        setNumber('')}}>AC</button>
+          <button onClick={()=> handleButtonClick('/')}>/</button>
+          <button onClick={() => handleButtonClick('=')}>=</button>
+          <button onClick={()=> handleButtonClick('AC')}>AC</button>
         </div>
+      </div>
       </div>
     </article>
   );
