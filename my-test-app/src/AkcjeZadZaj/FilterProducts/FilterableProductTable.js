2357e1d53de89b0d438e4cef96cb6e2d67ddf3d5
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-07 14:22:20 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/FilterProducts/FilterableProductTable.js
+++ my-test-app/src/AkcjeZadZaj/FilterProducts/FilterableProductTable.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { SearchBar } from './SearchBar'
-import { ProductTable } from './ProductTable'
-
-
+import React from "react";
+import { SearchBar } from "./SearchBar";
+import { ProductTable } from "./ProductTable";
 
 export const FilterableProductTable = () => {
-    return <article style={{marginLeft: 30, marginRight: 30}}>
-        <h4>FilterableProductTable</h4>
+  return (
+    <article style={{ marginLeft: 30, marginRight: 30 }}>
+      <h4>FilterableProductTable</h4>
+      <div style={{display: "flex", justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{width: 400, border: '2px solid yellow', padding:30, margin: 10}}>
         <SearchBar />
         <ProductTable />
+      </div>
+      </div>
+      
     </article>
-}
+  );
+};
