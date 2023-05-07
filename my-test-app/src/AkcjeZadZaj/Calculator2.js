c798fc9ea57f976f5f5b1ca86e83267349989aa8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-07 12:47:53 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/Calculator2.js
+++ my-test-app/src/AkcjeZadZaj/Calculator2.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Calculator2 = () => {
+    const [number, setNumber] = useState(0)
+    const [operation, setOpration] = useState('')
   return (
     <article style={{ marginLeft: 30, marginRight: 30 }}>
       <h4>Kalkulator 2</h4>
       <div style={{width: 400, backgroundColor: "gray", padding: 20, margin: 20, borderRadius: '10px'}}>
-        <input type="text" name="" id="" readOnly/>
+        <input type="text" name="" id="" readOnly value={`${number}${operation}${number}`} />
         <div style={{ display: "flex", gap: 20 }}>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <button onClick={()=> setNumber(1)}>1</button>
+          <button onClick={() => setNumber(2)}>2</button>
+          <button onClick={() => setNumber(3)}>3</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
+          <button onClick={()=> setNumber(4)}>4</button>
+          <button onClick={() => setNumber(5)}>5</button>
+          <button onClick={() => setNumber(6)}>6</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button onClick={()=> setNumber(7)}>7</button>
+          <button onClick={() => setNumber(8)}>8</button>
+          <button onClick={() => setNumber(9)}>9</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
-          <button>+</button>
+          <button onClick={()=> setOpration('+')}>+</button>
           <button>-</button>
           <button>*</button>
         </div>
         <div style={{ display: "flex", gap: 20 }}>
           <button>/</button>
           <button>=</button>
-          <button>C</button>
+          <button>AC</button>
         </div>
       </div>
     </article>
