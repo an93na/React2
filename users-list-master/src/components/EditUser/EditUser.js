1209c9d4d23c90cd2f3b1c23f03a6d7915842efb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-09 17:48:09 +0200

    changes

--- users-list-master/src/components/EditUser/EditUser.js
+++ users-list-master/src/components/EditUser/EditUser.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
-export const EditUser = () => {
+export const EditUser = (props) => {
+    const { users, setUsers } = props;
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
+    
     return <div>
-        
+
     </div>
 }
