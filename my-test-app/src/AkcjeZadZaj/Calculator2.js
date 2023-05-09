eb1fd3f04297b0e0187e036cde56f4e04f76d412
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-09 14:09:23 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/Calculator2.js
+++ my-test-app/src/AkcjeZadZaj/Calculator2.js
@@ -78,26 +78,24 @@ export const Calculator2 = () => {
             <button onClick={() => handleButtonClick(1)}>1</button>
             <button onClick={() => handleButtonClick(2)}>2</button>
             <button onClick={() => handleButtonClick(3)}>3</button>
+            <button onClick={() => handleButtonClick(4)}>4</button>
           </div>
           <div style={{ display: "flex", gap: 20 }}>
-            <button onClick={() => handleButtonClick(4)}>4</button>
             <button onClick={() => handleButtonClick(5)}>5</button>
             <button onClick={() => handleButtonClick(6)}>6</button>
-          </div>
-          <div style={{ display: "flex", gap: 20 }}>
             <button onClick={() => handleButtonClick(7)}>7</button>
             <button onClick={() => handleButtonClick(8)}>8</button>
-            <button onClick={() => handleButtonClick(9)}>9</button>
           </div>
           <div style={{ display: "flex", gap: 20 }}>
+            <button onClick={() => handleButtonClick(9)}>9</button>
+            <button onClick={() => handleButtonClick(0)}>0</button>
             <button onClick={() => handleButtonOperationClick("+")}>+</button>
             <button onClick={() => handleButtonOperationClick("-")}>-</button>
-            <button onClick={() => handleButtonOperationClick("*")}>*</button>
           </div>
           <div style={{ display: "flex", gap: 20 }}>
+            <button onClick={() => handleButtonOperationClick("*")}>*</button>
             <button onClick={() => handleButtonOperationClick("/")}>/</button>
             <button onClick={handleButtonResultClick}>=</button>
-            <button onClick={()=> handleButtonClick(0)}>0</button>
             <button onClick={handleButtonACClick}>AC</button>
           </div>
         </div>
