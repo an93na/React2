76db875c1ba2b985bbba0a40a66689a8867292ad
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-12 20:11:49 +0200

    changes Kształt

--- my-test-app/src/AkcjeZadZaj/Kolko.js
+++ my-test-app/src/AkcjeZadZaj/Kolko.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export const Kolko = () => {
   const [colorTla, setColorTla] = useState("blue");
-  const [ksztalt, setKsztalt] = useState("");
+  const [ksztalt, setKsztalt] = useState("0");
   const [polozenie, setPolozenie] = useState("");
   const [colorRamki, setColorRamki] = useState("");
   const [gruboscRamki, setGruboscRamki] = useState("");
@@ -13,8 +13,11 @@ export const Kolko = () => {
       <div style={{display: "flex", gap: 30, margin: 20}}>
         <h5>wybierz kolor tła: </h5><input type="color" name="" id="" style={{width: 100}} value={colorTla} onChange={(event) => setColorTla(event.target.value) }/>
       </div>
+      <div style={{display: "flex", gap: 30, margin: 20}}>
+        <h5>wybierz kształt: </h5><input type="range" name="" id="" style={{width: 150}} value={ksztalt} onChange={(event) => setKsztalt(event.target.value) }/>
+      </div>
       
-      <div style={{width:100, height:100, backgroundColor: colorTla, }}></div>
+      <div style={{width:100, height:100, backgroundColor: colorTla, borderRadius:`${ksztalt}px`}}></div>
     </article>
   );
 };
