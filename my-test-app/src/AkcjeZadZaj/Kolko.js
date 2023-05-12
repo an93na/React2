fc0c8e28e3fcc9fe74d2f4f50556f2f4bfc91ed3
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-12 20:09:08 +0200

    changes colorTla

--- my-test-app/src/AkcjeZadZaj/Kolko.js
+++ my-test-app/src/AkcjeZadZaj/Kolko.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export const Kolko = () => {
-  const [colorTla, setColorTla] = useState("");
+  const [colorTla, setColorTla] = useState("blue");
   const [ksztalt, setKsztalt] = useState("");
   const [polozenie, setPolozenie] = useState("");
   const [colorRamki, setColorRamki] = useState("");
@@ -10,7 +10,11 @@ export const Kolko = () => {
   return (
     <article>
       <h4>Stylowanie elementu</h4>
-      <div></div>
+      <div style={{display: "flex", gap: 30, margin: 20}}>
+        <h5>wybierz kolor tła: </h5><input type="color" name="" id="" style={{width: 100}} value={colorTla} onChange={(event) => setColorTla(event.target.value) }/>
+      </div>
+      
+      <div style={{width:100, height:100, backgroundColor: colorTla, }}></div>
     </article>
   );
 };
