3e252f4b3c7d980b3f8e35de3a4ad363d16a0742
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-12 20:01:30 +0200

    dodanie zmiennych

--- my-test-app/src/AkcjeZadZaj/Kolko.js
+++ my-test-app/src/AkcjeZadZaj/Kolko.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
-
+import React, { useState } from "react";
 
 export const Kolko = () => {
-    return <article>
-        <div></div>
+  const [colorTla, setColorTla] = useState("");
+  const [ksztalt, setKsztalt] = useState("");
+  const [polozenie, setPolozenie] = useState("");
+  const [colorRamki, setColorRamki] = useState("");
+  const [gruboscRamki, setGruboscRamki] = useState("");
+
+  return (
+    <article>
+      <h4>Stylowanie elementu</h4>
+      <div></div>
     </article>
-}
+  );
+};
