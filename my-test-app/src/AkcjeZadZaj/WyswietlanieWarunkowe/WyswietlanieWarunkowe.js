affa61fcdcabb09618bbe95553fb1beb2b9b2855
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-13 19:40:53 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/WyswietlanieWarunkowe/WyswietlanieWarunkowe.js
+++ my-test-app/src/AkcjeZadZaj/WyswietlanieWarunkowe/WyswietlanieWarunkowe.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 export const WyswietlanieWarunkowe = () => {
+    const [checked, setChecked] = useState(false)
     return <article>
         <h4>Wyswietlanie Warunkowe</h4>
+        <select name="" id="">
+            <option value="" hidden>Wybierz komponent</option>
+            <option value="cmp1">Component 1</option>
+            <option value="cmp2">Component 2</option>
+            <option value="cmp3">Component 3</option>
+        </select>
     </article>
 }
