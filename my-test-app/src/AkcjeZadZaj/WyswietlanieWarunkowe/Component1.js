6c88450445fdcc9b083c9d37b78f7f0e5212298b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-13 19:45:46 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/WyswietlanieWarunkowe/Component1.js
+++ my-test-app/src/AkcjeZadZaj/WyswietlanieWarunkowe/Component1.js
@@ -3,5 +3,5 @@ import React from "react";
 
 
 export const Component1 = () => {
-    return <div></div>
+    return <div style={{width:150, height:150, backgroundColor:'red', textAlign: 'center', alignItems: 'center'}}>Component1</div>
 }
