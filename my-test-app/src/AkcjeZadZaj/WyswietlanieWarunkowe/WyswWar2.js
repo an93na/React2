12ab1f6cfd6769e5429548f8b5706d8a46d380cd
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-14 14:17:14 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/WyswietlanieWarunkowe/WyswWar2.js
+++ my-test-app/src/AkcjeZadZaj/WyswietlanieWarunkowe/WyswWar2.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from "react";
 
 
 
 export const WyswWar2 = () => {
-    return <article>
-        
-    </article>
+   const [checked, setChecked] = useState(false)
+    return (<article>
+        <h4>Wyświetlanie Warunkowe2</h4>
+    </article>)
 }
