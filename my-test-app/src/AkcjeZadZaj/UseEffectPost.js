c50110c4462210125f4fa5d583ad12d73cf2db18
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-19 19:18:10 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/UseEffectPost.js
+++ my-test-app/src/AkcjeZadZaj/UseEffectPost.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 export const UseEffectPost = () => {
+    const [checked, setChecked]=useState(false);
+
     return <article>
-        
+        <label htmlFor="">
+            <input type="checkbox" name="" id="" checked={checked} onChange={()=> setChecked((prevState) => !prevState)}/>
+            Pokaż posty
+        </label>
     </article>
 }
