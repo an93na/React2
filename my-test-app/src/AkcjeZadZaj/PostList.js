54d7447cad80f58c2d24367fba260b02bc009f89
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-19 19:36:07 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/PostList.js
+++ my-test-app/src/AkcjeZadZaj/PostList.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 export const PostList = () => {
     const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState([]);
 
     useEffect(()=> {
         fetch("https://jsonplaceholder.typicode.com/posts")
@@ -16,6 +17,7 @@ export const PostList = () => {
     // console.log(posts)
     return <div style={{padding: 10, margin: 10}}>
         <h4>Post List</h4>
+        <input type="text" name="" id="" placeholder="Search" value={search} onChange={(event) => setSearch(event.target.value)}/>
         <ol style={{textAlign: 'left'}}>
             {posts.map((post) => {
                 return <li key={post.id}>{post.title}</li>
