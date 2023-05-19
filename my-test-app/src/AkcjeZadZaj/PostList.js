932eecc3f73d88302fd370f0cae6868551e084ae
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-19 19:56:41 +0200

    changes

--- my-test-app/src/AkcjeZadZaj/PostList.js
+++ my-test-app/src/AkcjeZadZaj/PostList.js
@@ -1,28 +1,51 @@
 import React, { useEffect, useState } from "react";
 
-
-
 export const PostList = () => {
-    const [posts, setPosts] = useState([]);
-    const [search, setSearch] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
 
-    useEffect(()=> {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => response.json())
-        .then((data) => {
-            setPosts(data)
-        })
-    }, [])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
 
-    // console.log(posts)
-    return <div style={{padding: 10, margin: 10}}>
-        <h4>Post List</h4>
-        <input type="text" name="" id="" placeholder="Search" value={search} onChange={(event) => setSearch(event.target.value)}/>
-        <ol style={{textAlign: 'left'}}>
-            {posts.map((post) => {
-                return <li key={post.id}>{post.title}</li>
-            })}
-        </ol>
-
+  // console.log(posts)
+  return (
+    <div style={{ padding: 10, margin: 10 }}>
+      <h4>Post List</h4>
+      <div style={{ display: "flex", gap: 20 }}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+      <ol style={{ textAlign: "left" }}>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              {post.title}
+              <button
+                style={{ width: "fit-content" }}
+                onClick={() => {
+                  const newList = posts.filter((postToDelete) => {
+                    return postToDelete.id !== post.id;
+                  });
+                  setPosts(newList);
+                }}
+              >
+                usun
+              </button>
+            </li>
+          );
+        })}
+      </ol>
     </div>
-}
+  );
+};
