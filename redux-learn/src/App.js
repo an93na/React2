f6c67ea2a979fb8ac71b20db5896e4762129408d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-22 15:22:30 +0200

    changes

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
+import "@picocss/pico";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
