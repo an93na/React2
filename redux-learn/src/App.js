1210d070c7ee113136d73c67aecaa4d78568e90b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-24 11:16:28 +0200

    changes

--- redux-learn/src/App.js
+++ redux-learn/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import "@picocss/pico";
 
+const style = {
+  margin: 20,
+  padding: 20,
+  border: "2px solid blue",
+  display: "flex",
+  borderRadius: 5,
+  gap: 50,
+};
+
 function App() {
-  return <div className="App">
-    
+  return <div className="container">
+    <article>
+      
+    </article>
   </div>;
 }
 
