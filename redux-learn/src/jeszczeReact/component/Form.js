88190406cecf6e2f38f6685134cab58d275a70d7
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-24 10:58:13 +0200

    changes

--- redux-learn/src/jeszczeReact/component/Form.js
+++ redux-learn/src/jeszczeReact/component/Form.js
@@ -1,9 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 export const Form = () => {
-    return <form action="">
-        <input type="text" name="" id="" />
-        <button>Submit</button>
+  const [title, setTitle] = useState("");
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3010/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    }).then(() => {
+      setTitle("");
+    });
+ }
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <input type="text" name="" id="" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title"/>
+      <button type="submit">Submit</button>
     </form>
-}
+  );
+};
