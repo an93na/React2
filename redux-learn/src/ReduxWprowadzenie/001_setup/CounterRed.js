63f756339564c2d69245cf5de9fa7fe827000ba4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-25 18:41:37 +0200

    changes

--- redux-learn/src/ReduxWprowadzenie/001_setup/CounterRed.js
+++ redux-learn/src/ReduxWprowadzenie/001_setup/CounterRed.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { increment, decrement, resetCounter, toodleShowPoweredValue } from "./counterSlice";
+import {
+  increment,
+  decrement,
+  resetCounter,
+  toodleShowPoweredValue,
+} from "./counterSlice";
 
 export const CounterRed = () => {
   const counter = useSelector((state) => state.counter.value);
-  const showPoweredValue = useSelector((state) => state.counter.showPoweredValue);
+  const poweredCounter = useSelector((state) => state.counter.poweredValue);
+  const showPoweredValue = useSelector(
+    (state) => state.counter.showPoweredValue
+  );
   const dispatch = useDispatch();
   return (
     <div>
@@ -14,10 +22,16 @@ export const CounterRed = () => {
       <button onClick={() => dispatch(decrement())}>-1</button>
       <button onClick={() => dispatch(resetCounter())}>RESET</button>
       <label>
-        <input type="checkbox" name="" id="" checked={showPoweredValue} onChange={()=>dispatch(toodleShowPoweredValue())}/> Show
-        Powered Value
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          checked={showPoweredValue}
+          onChange={() => dispatch(toodleShowPoweredValue())}
+        />{" "}
+        Show Powered Value
       </label>
-      {showPoweredValue && <p>Lorem ipsum dolor sit amet.</p>}
+      {showPoweredValue && <p>Powered value: {poweredCounter}</p>}
     </div>
   );
 };
--- redux-learn/src/ReduxWprowadzenie/001_setup/counterSlice.js
+++ redux-learn/src/ReduxWprowadzenie/001_setup/counterSlice.js
@@ -16,7 +16,8 @@ const counterSice = createSlice({
       state.poweredValue = state.value * state.value;
     },
     decrement: (state) => {
-      state.value = state.value - 1;
+        state.value = state.value - 1;
+        state.poweredValue = state.value * state.value;
     },
     resetCounter: (state) => {
       state.value = 0;
