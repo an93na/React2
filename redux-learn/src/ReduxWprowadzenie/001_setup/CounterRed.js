20c55b4db6f3054b5232bfafbdd0cf236142a0ad
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-25 18:37:32 +0200

    changes

--- redux-learn/src/ReduxWprowadzenie/001_setup/CounterRed.js
+++ redux-learn/src/ReduxWprowadzenie/001_setup/CounterRed.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { increment, decrement, resetCounter } from './counterSlice'
+import { increment, decrement, resetCounter, toodleShowPoweredValue } from "./counterSlice";
 
 export const CounterRed = () => {
-    const counter = useSelector((state) => state.counter.value)
-    const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.value);
+  const showPoweredValue = useSelector((state) => state.counter.showPoweredValue);
+  const dispatch = useDispatch();
   return (
     <div>
-        <button onClick={()=> dispatch(increment())}>+1</button>
-        <input type="text" name="" id="" readOnly value={counter}/>
-        <button onClick={()=> dispatch(decrement())}>-1</button>
-        <button onClick={()=> dispatch(resetCounter())}>RESET</button>
-        </div>
-  )
-}
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <input type="text" name="" id="" readOnly value={counter} />
+      <button onClick={() => dispatch(decrement())}>-1</button>
+      <button onClick={() => dispatch(resetCounter())}>RESET</button>
+      <label>
+        <input type="checkbox" name="" id="" checked={showPoweredValue} onChange={()=>dispatch(toodleShowPoweredValue())}/> Show
+        Powered Value
+      </label>
+      {showPoweredValue && <p>Lorem ipsum dolor sit amet.</p>}
+    </div>
+  );
+};
--- redux-learn/src/ReduxWprowadzenie/001_setup/counterSlice.js
+++ redux-learn/src/ReduxWprowadzenie/001_setup/counterSlice.js
@@ -6,21 +6,27 @@ const counterSice = createSlice({
   name: "counter", //nazwa slice, ja je wybieram
   initialState: {
     value: 0, //stan początkowy tak jak w useState(0)
+    poweredValue: 0,
+    showPoweredValue: false,
   },
   //funkcje które mówią w jaki sposób możemy zaktualizować nasz stan
   reducers: {
     increment: (state) => {
       state.value = state.value + 1;
+      state.poweredValue = state.value * state.value;
     },
     decrement: (state) => {
       state.value = state.value - 1;
     },
     resetCounter: (state) => {
-        state.value = 0
+      state.value = 0;
+    },
+    toodleShowPoweredValue: (state) => {
+        state.showPoweredValue = !state.showPoweredValue
     }
   },
 });
 
-export const {decrement, increment, resetCounter} = counterSice.actions
+export const { decrement, increment, resetCounter, toodleShowPoweredValue } = counterSice.actions;
 
 export default counterSice.reducer;
