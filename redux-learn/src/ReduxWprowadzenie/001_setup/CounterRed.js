a76adc39cac8d26163325f32452a34c52e945327
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-25 18:03:30 +0200

    changes

--- redux-learn/src/ReduxWprowadzenie/001_setup/CounterRed.js
+++ redux-learn/src/ReduxWprowadzenie/001_setup/CounterRed.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { increment, decrement } from './counterSlice'
 
 export const CounterRed = () => {
     const counter = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch();
   return (
     <div>
-        <button>+1</button>
+        <button onClick={()=> dispatch(increment())}>+1</button>
         <input type="text" name="" id="" readOnly value={counter}/>
-        <button>-1</button>
+        <button onClick={()=> dispatch(decrement())}>-1</button>
         </div>
   )
 }
