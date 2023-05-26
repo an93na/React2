580987c0cf574a85a06a901beb52beed28d9f847
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-26 11:21:00 +0200

    changes

--- redux-learn/src/ReduxWprowadzenie/001_setup/ComplexCounterWithRedux/complexCounterSlice.js
+++ redux-learn/src/ReduxWprowadzenie/001_setup/ComplexCounterWithRedux/complexCounterSlice.js
@@ -1,7 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 const complexCounterSlice = createSlice({
-    
-})
+  name: "counter3",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increaseComplex: (state, action) => {
+      state.value = state.value + action.payload;
+    },
+    decreaseComplex: (state, action) => {
+      state.value = state.value - action.payload;
+    },
+  },
+});
+
+export const complexSelectVal = (state) => state.counter3.value;
+export const {increaseComplex, decreaseComplex} = complexCounterSlice.actions
+export default complexCounterSlice.reducer;
--- redux-learn/src/store/store.js
+++ redux-learn/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../ReduxWprowadzenie/001_setup/counterSlice";
 import counterReducer2 from "../ReduxWprowadzenie/001_setup/counterSlice2";
+import counterComplexReducer from "../ReduxWprowadzenie/001_setup/ComplexCounterWithRedux/complexCounterSlice";
 // torzenie stora - obiektu trzymającego stan
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     counter2: counterReducer2,
+    counter3: counterComplexReducer,
   },
 });
