3b9e717a899b1e534024010c66f9d628ff635816
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-27 18:20:06 +0200

    changes

--- redux-learn/src/ReduxWprowadzenie/002_TodoList/Form.js
+++ redux-learn/src/ReduxWprowadzenie/002_TodoList/Form.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Form = () => {
+  const [todos, setTodos] = useState("");
   return (
-    <div>Form</div>
-  )
-}
+    <div>
+      <form action="" onSubmit={(e) => {
+        e.preventDefault()
+
+      }}>
+        <input type="text" placeholder="Todo" name="" id="" value={todos} onChange={(e) => setTodos(e.target.value)}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
