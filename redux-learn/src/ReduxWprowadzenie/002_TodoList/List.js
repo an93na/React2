f43f78304a9ad9b160765c8aecbd2d854f88dec2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-27 18:35:33 +0200

    changes

--- redux-learn/src/ReduxWprowadzenie/002_TodoList/List.js
+++ redux-learn/src/ReduxWprowadzenie/002_TodoList/List.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectTodos2 } from './sliceTodoList2'
 
 export const List = () => {
-  return (
-    <div>List</div>
+ const todoList = useSelector(selectTodos2)
+    return (
+    <div>
+        <ul>
+            <li style={{listStyle: 'none'}}>TODOS: </li>
+            {todoList.map((todo) => <li key={todo}>{todo}</li>)}
+        </ul>
+    </div>
   )
 }
