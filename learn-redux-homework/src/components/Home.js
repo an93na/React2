90d6b5905aab19270690d48075758931c68ed31e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-30 13:19:58 +0200

    changes

--- learn-redux-homework/src/components/Home.js
+++ learn-redux-homework/src/components/Home.js
@@ -5,6 +5,7 @@ export const Home = () => {
     <div>
       <h3>Home</h3>
       <p>Home page of redux exercises</p>
+      <p>Ćwiczenie z repozytorium: https://github.com/simon125/react-redux-react-router-dom.git </p>
     </div>
 
   )
