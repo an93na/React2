3094e90e0b7d31ca5214f75aaeeafd15f62a6d37
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-30 13:17:28 +0200

    changes

--- learn-redux-homework/src/components/Dashboard.js
+++ learn-redux-homework/src/components/Dashboard.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectCosTamCosTam } from "./counterSlice";
+import { selectCountInput } from "./CounterWithInputSlice";
 
-export const Dashboard = (value) => {
+export const Dashboard = () => {
+  const counter = useSelector(selectCosTamCosTam);
+  const counterInp = useSelector(selectCountInput);
   return (
     <div>
       <h3>Dashboard</h3>
-      <p>Counter value: {} </p>
-      <p>Counter with input value: {} </p>
+      <p>Counter value: {counter} </p>
+      <p>Counter with input value: {counterInp} </p>
     </div>
   );
 };
