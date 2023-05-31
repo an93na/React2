8d376ea416babf6f45fa8882f82af125ef520783
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-31 14:56:56 +0200

    changes

--- learn-redux-homework/src/zajecia_comp/Metrics.js
+++ learn-redux-homework/src/zajecia_comp/Metrics.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectTotalNumberMarksofTodos, selectTotalNumberofTodos } from "./todoListSlice";
+import {
+  selectTotalNumberLessOf5MarksTodos,
+  selectTotalNumberMarksofTodos,
+  selectTotalNumberofTodos,
+} from "./todoListSlice";
 
 export const Metrics = () => {
   const total = useSelector(selectTotalNumberofTodos);
   const znaki = useSelector(selectTotalNumberMarksofTodos);
+  const mniejniz5 = useSelector(selectTotalNumberLessOf5MarksTodos);
   return (
     <article>
       <p>Total: {total}</p>
       <p>Ilość wszystkich znaków: {znaki}</p>
-      <p>Ilość słów które mają mniej niż 5 znaków: {10}</p>
+      <p>Ilość słów które mają mniej niż 5 znaków: {mniejniz5}</p>
     </article>
   );
 };
--- learn-redux-homework/src/zajecia_comp/todoListSlice.js
+++ learn-redux-homework/src/zajecia_comp/todoListSlice.js
@@ -23,7 +23,11 @@ export const selectTodo = (state) => state.todoList.todos;
 
 export const selectTotalNumberofTodos = (state) => state.todoList.todos.length;
 
-export const selectTotalNumberMarksofTodos = (state) => state.todoList.todos.join('').length;
+export const selectTotalNumberMarksofTodos = (state) =>
+  state.todoList.todos.join("").length;
+
+export const selectTotalNumberLessOf5MarksTodos = (state) =>
+  state.todoList.todos.filter((todo) => todo.length < 5).length;
 
 export const { addTodo, resetState } = todoListSlice.actions;
 
