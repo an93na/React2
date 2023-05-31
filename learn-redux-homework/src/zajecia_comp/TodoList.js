da03d856a9d78f7f8b42be75ce61956e5115db1f
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-31 15:08:36 +0200

    changes

--- learn-redux-homework/src/zajecia_comp/TodoList.js
+++ learn-redux-homework/src/zajecia_comp/TodoList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Form } from "./Form";
 import { List } from "./List";
+import { Search } from "./Search";
 
 export const TodoList = () => {
   return (
     <article>
       <p>TodoList</p>
       <Form/>
+      <Search/>
       <List/>
     </article>
   );
