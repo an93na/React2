d74d5dc78dd6e4f1d6f0216f8128a1ce3b4245da
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-01 12:04:56 +0200

    changes

--- learn-redux-homework/src/zajecia_comp/zad1/UsersList.js
+++ learn-redux-homework/src/zajecia_comp/zad1/UsersList.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
 export const UsersList = () => {
+    const users = [];
+
   return (
-    <div>UsersList</div>
-  )
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Last Name</th>
+          <th>Age</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => {
+          return (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.lastName}</td>
+              <td>{user.age}</td>
+              <td>
+                <button>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
