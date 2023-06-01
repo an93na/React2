b222f14f8ad30c056b2bd4280f7683eb7246b42d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-01 12:23:01 +0200

    changes

--- learn-redux-homework/src/zajecia_comp/zad1/UseForm.js
+++ learn-redux-homework/src/zajecia_comp/zad1/UseForm.js
@@ -29,7 +29,7 @@ export const UseForm = () => {
             age,
           };
           dispatch(addUser(user));
-          setLastName("");
+          setName("");
           setLastName("");
           setAge("");
         }}
