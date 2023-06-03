2559562b95324009caafcb83f5ad8538e30efd3a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-03 13:12:38 +0200

    changes

--- testing-react-app/src/components/LoginComponents/Login.test.js
+++ testing-react-app/src/components/LoginComponents/Login.test.js
@@ -1,28 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 
-
-
 describe("Login component", () => {
-  test("should be username text in placeholder", () => {
-    // wchodzimy do pliku Login
-    render(<Login />);
-    // sprawdzam czy napis 'username istnieje w palceholderze
-    const usernameInputPlaceholder = screen.getByPlaceholderText("username");
-    // oczekuje, że to powyżej znajduje się na naszje stronie
-    expect(usernameInputPlaceholder).toBeInTheDocument();
-  });
 
-  test("should be password text in placeholder", () => {
-    render(<Login />);
-    const passwordInputPlaceholder = screen.getByPlaceholderText("password");
-    expect(passwordInputPlaceholder).toBeInTheDocument();
-  });
+    test("should be username text in placeholder", () => {
+        // wchodzimy do pliku Login
+        render(<Login />);
+        // sprawdzamy czy napis 'username' wystepuje w placeholderze
+        const usernameInputPlaceholder = screen.getByPlaceholderText("username");
+        // oczekuje, że to powyżej znajduje się na naszej stronie
+        expect(usernameInputPlaceholder).toBeInTheDocument();
+    });
 
-  test("should rendered button", () => {
-    render(<Login />);
-    // getByRole - sprawdzamy role elementu czyli czy może istnieć button, input, form
-    const buttonElement = screen.getByRole("button");
-    expect(buttonElement).toBeInTheDocument();
-});
+    test("should be password text in placeholder", () => {
+        render(<Login />);
+        const passwordInputPlaceholder = screen.getByPlaceholderText("password");
+        expect(passwordInputPlaceholder).toBeInTheDocument();
+    });
+    
+    test("should rendered button", () => {
+        render(<Login />);
+        // getByRole - sprawdzamy role elementu czyli czy może istnieć button, input, form
+        const buttonElement = screen.getByRole("button");
+        expect(buttonElement).toBeInTheDocument();
+    });
+
+    test("should be empty for username input", () => {
+        render(<Login />);
+        const usernameInput = screen.getByPlaceholderText("username");
+        expect(usernameInput.value).toBe("");
+    });
 });
