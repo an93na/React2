f7ed805c99718c1963953909278c826475fa09f0
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-03 11:42:32 +0200

    changes

---
+++ testing-react-app/src/components/LoginComponents/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="" id="" placeholder="username" />
+        <input type="password" name="" id="" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
