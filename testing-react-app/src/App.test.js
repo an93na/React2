615ff2bb62d50cbf2573c0b3fa53ea130e2e683e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-03 11:21:37 +0200

    changes

--- testing-react-app/src/App.test.js
+++ testing-react-app/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// robimy test i nazywamy go
 test('renders learn react link', () => {
+  //mówimy co chcemy przetestować (render), wchodzimy do pliku App i odczytujemy go
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  //chcemy zobaczyć czy napis Learn react się wyświetla. Screen pochodzi z reactowej biblioteki jest to metoda
+  // co mamy na ekranie, co się wyświetla i getByText bierzemy cały tekst i sprawdzamy czy istnieje ten /tekst/ 
+  const linkElement = screen.getByText(/Learn Programing/i);
+  // oczekujemy czy ten tekst istnieje w tym dokumencie 
   expect(linkElement).toBeInTheDocument();
 });
