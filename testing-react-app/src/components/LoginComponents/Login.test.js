32cc9f55ef98031e7c70233525eecc7090952d32
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-03 12:35:38 +0200

    changes

--- testing-react-app/src/components/LoginComponents/Login.test.js
+++ testing-react-app/src/components/LoginComponents/Login.test.js
@@ -2,13 +2,18 @@ import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 
 describe("Login component", () => {
+  test("should be username text in placeholder", () => {
+    // wchodzimy do pliku Login
+    render(<Login />);
+    // sprawdzam czy napis 'username istnieje w palceholderze
+    const usernameInputPlaceholder = screen.getByPlaceholderText("username");
+    // oczekuje, że to powyżej znajduje się na naszje stronie
+    expect(usernameInputPlaceholder).toBeInTheDocument();
+  });
 
-    test("should be username text in placeholder", () => {
-        // wchodzimy do pliku Login
-        render(<Login />);
-        // sprawdzam 
-        const usernameInputPlaceholder = screen.getByPlaceholderText("username");
-        // 
-        expect(usernameInputPlaceholder).toBeInTheDocument();
-    })
+  test("should be password text in placeholder", () => {
+    render(<Login />);
+    const passwordInputPlaceholder = screen.getByPlaceholderText("password");
+    expect(passwordInputPlaceholder).toBeInTheDocument();
+  });
 });
