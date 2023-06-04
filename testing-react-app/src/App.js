46af21521b84889fe98816b26b501feef9bb7ae6
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-04 10:49:33 +0200

    changes

--- testing-react-app/src/App.js
+++ testing-react-app/src/App.js
@@ -1,10 +1,12 @@
 import Login from './components/LoginComponents/Login'
 import './App.css';
+import Submit from './components/SubmitComponent/Submit'
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Submit/>
     </div>
   );
 }
---
+++ testing-react-app/src/components/SubmitComponent/Submit.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Submit() {
+  const [text, setText] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
+
+  return (
+    <div className="Submit">
+      {submitted ? <h1 id="submitted">Form Submitted</h1> : null}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Submit;
