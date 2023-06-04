08fa8e0abb4b964cc2e7a283a161cfe3557fcbfe
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-04 14:37:24 +0200

    changes

--- testing-react-app/src/components/SubmitComponent/Submit.jsx
+++ testing-react-app/src/components/SubmitComponent/Submit.jsx
@@ -14,6 +14,7 @@ function Submit() {
       {submitted ? <h1 id="submitted">Form Submitted</h1> : null}
       <form onSubmit={handleSubmit}>
         <input
+        required
           type="text"
           id="text"
           value={text}
