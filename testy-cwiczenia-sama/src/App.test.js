5b0c5216d8387d4a97a71a3a5fada9ad66f61dd2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-05 13:11:21 +0200

    changes

--- testy-cwiczenia-sama/src/App.test.js
+++ testy-cwiczenia-sama/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// 'nazywamy sobie jakos ten nasz test'
 test('renders learn react link', () => {
+  // render oznacza, że wchodzimy do pliku App
   render(<App />);
+  // screen co mamy na ekranie jeeśli coś nam się wyświetla 
+  // to bierzemy ten tekst czyli 
+  // getByText(/tutaj tekst, który sprawdzamy czy istnieje/i)
   const linkElement = screen.getByText(/learn react/i);
+  // expect oczekujemy że element LinkElement 
+  // znajduje się w naszym dokumencie
   expect(linkElement).toBeInTheDocument();
 });
