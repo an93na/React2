b66e36347b1e31df2d1d5ad3c0ed9224dc82f2fc
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-05 14:29:20 +0200

    changes

--- testy-cwiczenia-sama/src/App.js
+++ testy-cwiczenia-sama/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./components/LoginComponents/Login";
 
 function App() {
   return (
     <div className="App">
-
+      <Login />
     </div>
   );
 }
