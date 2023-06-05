8e72363bde9226bd7b66b6d5fda2f8ec093b0465
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-05 14:33:23 +0200

    changes

---
+++ testy-cwiczenia-sama/src/components/LoginComponents/Login.jsx
@@ -0,0 +1,13 @@
+
+
+const Login = () => {
+  return <div className="container">
+    <form action="">
+        <input type="text" name="" id="" placeholder="username" />
+        <input type="password" name="" id="" placeholder="password" />
+        <button>Login</button>
+    </form>
+  </div>;
+};
+
+export default Login;
