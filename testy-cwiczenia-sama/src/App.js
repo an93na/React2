ac1191b075f5665bdf7b63e8e18b140fef094d23
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-15 19:45:33 +0200

    changes

--- testy-cwiczenia-sama/src/App.js
+++ testy-cwiczenia-sama/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Login from "./components/LoginComponents/Login";
 import Submit from "./components/SubmitComponent/Submit";
+import { TestData } from "./components/TestComponents/TestData";
 
 function App() {
   return (
     <div className="App">
       <Login />
       <Submit/>
+      <TestData/>
     </div>
   );
 }
--- testy-cwiczenia-sama/src/components/TestComponents/TestData.js
+++ testy-cwiczenia-sama/src/components/TestComponents/TestData.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 
-
 export const TestData = () => {
-    const [photo, setPhoto] = useState([]);
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-          .then((response) => response.json())
-          .then((data) => {
-            setPhoto(data);
-          });
-      }, []);
-    return <div>
-        <select name="" id="">
-            <option value="" hidden>Wybiesz zdjęcie</option>
-            {photo.map((fot)=> {
-                return <option value={fot}>{fot}</option>
-            })}
-        </select>
+  const [photo, setPhoto] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPhoto(data);
+      });
+  }, []);
+  return (
+    <div>
+      <select name="" id="">
+        <option value="" hidden>
+          Wybiesz zdjęcie
+        </option>
+        {photo.map((fot) => {
+          return <option value={fot.id}>{fot.title}</option>;
+        })}
+      </select>
     </div>
-}
+  );
+};
