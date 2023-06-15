de25516f6dd34fb95802a69ab6d4f505a8df4265
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-15 19:35:07 +0200

    changes

--- testy-cwiczenia-sama/src/components/TestComponents/TestData.js
+++ testy-cwiczenia-sama/src/components/TestComponents/TestData.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
 export const TestData = () => {
+    const [photo, setPhoto] = useState('');
     return <div>
-        
+        <select name="" id="">
+            <option value="" hidden>Wybiesz zdjęcie</option>
+        </select>
     </div>
 }
