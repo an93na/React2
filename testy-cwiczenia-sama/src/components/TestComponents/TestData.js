56d5224b4cdb2e5b05230586720c5e5c7f871b59
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-15 19:40:10 +0200

    changes

--- testy-cwiczenia-sama/src/components/TestComponents/TestData.js
+++ testy-cwiczenia-sama/src/components/TestComponents/TestData.js
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react";
 
 
 export const TestData = () => {
-    const [photo, setPhoto] = useState('');
+    const [photo, setPhoto] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+          .then((response) => response.json())
+          .then((data) => {
+            setPhoto(data);
+          });
+      }, []);
     return <div>
         <select name="" id="">
             <option value="" hidden>Wybiesz zdjęcie</option>
